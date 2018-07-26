f58a540feffd9eaf58385d07bfd2d46d9360b103
Harish Sunkerikar <urhari04@gmail.com>
2018-07-26 23:53:27 +0530

    Created Observer with Observable.create and async with setTimeout

--- src/app.js
+++ src/app.js
@@ -14,10 +14,41 @@ class MyObserver {
     }
 }
 
-source.subscribe(new MyObserver());
+//source.subscribe(new MyObserver());
 
 source.subscribe(
     (value) => console.log(`value: ${value}`),
     e => console.log(`error: ${e}`),
     () => console.log('completed')
 )
+
+//Creating observer with Observable.create
+let numbers2 = [7,8,9];
+// let source1 = Observable.create((observer) => {
+//     for(let n of numbers2) {
+//         if(n == 8) {
+//             observer.error('there is an error');
+//         }
+//         observer.next(n);
+//     }
+//     observer.complete();
+// })
+
+let source1 = Observable.create((observer) => {
+    let index = 0;
+    let produceValue = () => {
+        observer.next(numbers2[index++])
+        if(index < numbers2.length) {
+            setTimeout(produceValue, 1000);
+        } else {
+            observer.complete();
+        }
+    }
+    produceValue();
+});
+
+source1.subscribe(
+    (value) => console.log(`value from numbers2: ${value}`),
+    e => console.log(`error: ${e}`),
+    () => console.log('completed')
+);
